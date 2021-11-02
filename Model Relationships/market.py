ea81f498d987a4ef1b0a44073a274f1053dd21d3
zmanhcong <44230257+zmanhcong@users.noreply.github.com>
2021-11-03 01:05:27 +0900

    Models and Databases

---
+++ Model Relationships/market.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+#SQLALCHEMY_DATABASE_URI: is key name
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30),nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12),nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    #limit length of string, nullable=False:this is constraint, it mean NOT NULL in that column, unique=True  indicates that the Index should be created with the unique flag.(name not duplicate)
+
+
+    def __repr__(self):
+        return f'Item {self.name}'  #data duoc nhap tu commmand vao database -> khi ta them chuc nang này thì nó sẽ hiện tên chứ không hiện ra item.
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
