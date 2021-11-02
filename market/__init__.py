4724fdbc784c4ff57dc9f8495cab90445947b81e
zmanhcong <44230257+zmanhcong@users.noreply.github.com>
2021-11-02 14:11:09 +0900

    Delete market directory

--- market/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
-#app.config is config key name and create market.db::: SQLALCHEMY_DATABASE_URI: is key name
-
-from market import routes   #use this stament for connect with routes.
